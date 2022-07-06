ac450e4085fb75c56cd180bb395f9853f7f4a206
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-07-06 15:21:49 +0300

    first commit

---
+++ src/main/java/com/mateacademy/booksAndAuthors/BooksAndAuthorsApplication.java
@@ -0,0 +1,13 @@
+package com.mateacademy.booksAndAuthors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAndAuthorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAndAuthorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateacademy/booksAndAuthors/BooksAndAuthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateacademy.booksAndAuthors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAndAuthorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
